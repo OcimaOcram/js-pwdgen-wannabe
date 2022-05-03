532c9cbbebed81d7652c3c9e8b3e02d1e0249059
OcimaOcram <marco.amico.pesci00@gmail.com>
2022-05-03 15:44:50 +0200

    Esercizio completo

---
+++ script.js
@@ -0,0 +1,17 @@
+const nome = prompt("Qual'è il tuo nome?");
+
+console.log(nome);
+
+const cognome = prompt("Qual'è il tuo cognome?");
+
+console.log(cognome);
+
+const coloreP = prompt("Qual'è il tuo colore preferito?");
+
+console.log(coloreP);
+
+// Creazione password
+
+const password = nome + cognome + coloreP + "21"
+console.log(password);
+
